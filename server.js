11cf4aa4e2039933be9c80ad3699035958ab1676
Kevin Martinez Montero <kevinmm.19@gmail.com>
2022-02-18 03:50:20 +0100

    Convert server.js to ES module.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const serveStatic = require("serve-static")
-const path = require('path')
+import express from 'express'
+import serveStatic from 'serve-static'
+import { join } from 'path'
+const port = process.env.PORT || 80
 
 app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-const port = process.env.PORT || 80
+app.use(serveStatic(join(__dirname, 'dist')))
 app.listen(port)
